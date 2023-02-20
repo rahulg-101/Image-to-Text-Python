8583d2bedc044db8d4b7ae4b8149e8a7c3993b0b
rahulg-101 <123820143+rahulg-101@users.noreply.github.com>
2023-02-20 16:17:38 +0530

    Update img.py

--- img.py
+++ img.py
@@ -4,7 +4,7 @@ import cv2
 #Now we will read the image file from our directory (Note: Image should be in the same folder as your .py file)
 img = cv2.imread('2003.jpg')
 # Resizing of the image
-img = cv2.resize(img, (600, 360))
+img = cv2.resize(img, (720, 480))
 
 # Used to set the path for tesseract.exe file which will help in extracting the text from images
 pytesseract.pytesseract.tesseract_cmd =r'C:\Program Files\Tesseract-OCR\tesseract.exe'
